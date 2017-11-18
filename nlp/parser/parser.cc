6e0946efa31f8d4b4ea92b5b909fdf293ffd3e97
Dmytro Tkanov <akademi4eg@gmail.com>
2017-11-18 08:58:09 +0100

    fix for missing INFINITY constant (#89)

--- nlp/parser/parser.cc
+++ nlp/parser/parser.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <math.h>
+
 #include "nlp/parser/parser.h"
 
 #include "frame/serialization.h"
